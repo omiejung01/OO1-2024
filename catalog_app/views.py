b09807440f57c58555e86c530d48f85abe5c1629
Dhirachat Chayaporn <omiejung@gmail.com>
2024-09-23 14:31:03 +0700

    Rest API - List

--- catalog_app/views.py
+++ catalog_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Product
+from django.http import HttpResponseRedirect, HttpResponse
+import json
 
 # Create your views here.
 def homepage(request):
@@ -7,3 +10,30 @@ def homepage(request):
         'current_email': 'Not defined'
     }
     return render(request, 'homepage.html', context)
+
+def product_list(request):
+    #qs = PlayVideo.objects.all()
+    list = [{'id': x.product_id, 'title': x.product_title, 'price': str(x.display_price),
+             'image': str(x.product_pic),
+         }
+            for x in Product.objects.filter(void=0).order_by('-created_time')]
+    #qs_json = serializers.serialize('json', list)
+    qs_json = json.dumps(list)
+    return HttpResponse(qs_json, content_type='application/json')
+
+
+def product_detail(request):
+    id = request.GET.get('id')
+    list = [{
+
+             'id': x.product_id, 'title': x.product_title, 'price': str(x.display_price),
+        'unit': x.product_unit, 'description': str(x.description), 'image': str(x.product_pic),
+             'updated_time': x.updated_time.strftime('%Y-%m-%d %H:%M:%S'),
+             'created_time': x.created_time.strftime('%Y-%m-%d %H:%M:%S'),
+             }
+            for x in Product.objects.filter(product_id=id).filter(void=0).order_by('-created_time')]
+
+
+    qs_json = json.dumps(list[0])
+    return HttpResponse(qs_json, content_type='application/json')
+
