4d5916b8ff078899c5066d7401662a945fcf8900
Dhirachat Chayaporn <omiejung@gmail.com>
2024-09-23 21:07:42 +0700

    Rest API - Product detail

--- catalog_app/urls.py
+++ catalog_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 
     path('', views.homepage, name='homepage'),
-
+    path('product_list', views.product_list, name='product list'),
+    path('product_detail', views.product_detail, name='product detail'),
 ]
