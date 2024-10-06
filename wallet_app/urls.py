f23867dd1926bed061323c8b8bd1a414fa891a8f
Dhirachat Chayaporn <omiejung@gmail.com>
2024-10-06 16:31:52 +0700

    Q2 finished

---
+++ wallet_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.homepage, name='homepage'),
+    path('register_account', views.register_account, name='register_account'),
+    path('transfer', views.transfer, name='transfer'),
+    path('check_balance', views.check_balance, name='check_balance')
+
+]
